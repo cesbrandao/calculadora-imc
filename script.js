deae08d1665be7f6a0d9b44984f4fd5bbfa8da37
cesbrandao <ceduardosbrandao@gmail.com>
2024-08-20 15:25:47 -0300

    upadate

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 window.onload = function(){
+    //função ativada após o usuário clicar no botão Calcular
     document.getElementById("calculate-imc").addEventListener("click", function(){
+        //armazena o valor do campo Altura
         height = parseFloat(document.getElementById("height-input").value);
+        //armazena o valor do campo Peso
         weight = parseFloat(document.getElementById("weight-input").value);
 
-        if(isNaN(height) || isNaN(weight)){
-            alert("Insira valores válidos.");
-            return;
-        }
-
-        document.getElementById("imc-result").value = (weight / (height * height)).toFixed(2);
+        //retorna o valor do IMC calculado para o campo IMC
+        document.getElementById("imc-result").value = calculateIMC(height, weight);
     });
 }
+
+
+function calculateIMC(height, weight) {
+    //verifica se os valores são numéricos
+    if(isNaN(height) || isNaN(weight)){
+        alert("Insira valores válidos.");
+        return '';
+    } else {
+        //cálculo do IMC
+        return (weight / (height * height)).toFixed(2);
+    };
+};
